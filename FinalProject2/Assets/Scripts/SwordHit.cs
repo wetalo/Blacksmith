f4b68f5011f403e0a5a1314cb73e2ef246cd8859
Daniel Swieca <danielswieca@gmail.com>
2019-02-07 15:06:56 -0500

    Sword particle on hit

---
+++ FinalProject2/Assets/Scripts/SwordHit.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordHit : MonoBehaviour {
+
+
+    public GameObject hitParticle;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "HammerHead")
+        {
+
+            GameObject hitInstance = GameObject.Instantiate(hitParticle, other.ClosestPointOnBounds(transform.position), Quaternion.identity);
+            hitInstance.transform.rotation = Quaternion.LookRotation(transform.up,Vector3.up);
+
+        }
+    }
+}
