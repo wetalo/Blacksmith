6a1f1f26544955d8fbb857d132b1796d81eadd96
Daniel Swieca <danielswieca@gmail.com>
2019-03-15 13:54:44 -0400

    Teleporter

---
+++ FinalProject2/Assets/Scripts/Teleporter/Teleporter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour {
+
+    public TeleporterHitbox hitbox;
+    public Teleporter sisterTeleporter;
+
+    public Vector3 spawnLocation;
+
+	// Use this for initialization
+	void Start () {
+
+        
+	}
+	
+	// Update is called once per frame
+	public void Teleport()
+    {
+        if (hitbox.playerIsOnTeleporter)
+        {
+
+            GameObject player = GameObject.Find("LocalAvatarWithGrab");
+
+            spawnLocation = player.transform.position + (sisterTeleporter.gameObject.transform.position - transform.position);
+
+            player.transform.position = spawnLocation;
+        }
+    }
+}
---
+++ FinalProject2/Assets/Scripts/Teleporter/TeleporterHitbox.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleporterHitbox : MonoBehaviour {
+
+    public bool playerIsOnTeleporter = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.name == "PlayerHeadHitbox")
+        {
+            playerIsOnTeleporter = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "PlayerHeadHitbox")
+        {
+            playerIsOnTeleporter = false;
+        }
+    }
+
+
+}
