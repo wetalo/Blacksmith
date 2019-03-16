197b092851366d831b636b845a8c8d76196f0e93
Andreas <a.kapogiannatos@hotmail.com>
2019-03-15 21:25:07 -0400

    Trying some stuff with Ramy

---
+++ FinalProject2/Assets/Shader/Ripple.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ripple : MonoBehaviour {
+
+    public Material mat;
+	// Use this for initialization
+	void Start () {
+        mat.SetVector("_RipplePoint", new Vector2(0.25f, 0.5f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
