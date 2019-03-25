1ce6dd33b98103a13fe72918603ded18e8cf94ae
Daniel Swieca <danielswieca@gmail.com>
2019-03-25 16:16:31 -0400

    Added delay in hitting hitboxes.

--- FinalProject2/Assets/Scripts/SwordHit.cs
+++ FinalProject2/Assets/Scripts/SwordHit.cs
@@ -10,6 +10,10 @@ public class SwordHit : MonoBehaviour {
     public SongGenerator generator;
     public SwordHitboxType hitboxIndex;
 
+    bool isHittable = true;
+    public FloatVariable hitCooldownTime;
+    float hitCoolTimer = 0f;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -17,26 +21,41 @@ public class SwordHit : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (!isHittable)
+        {
+            hitCoolTimer += Time.deltaTime;
+            if(hitCoolTimer > hitCooldownTime.Value)
+            {
+                isHittable = true;
+                hitCoolTimer = 0f;
+            }
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "HammerHead")
+        if (other.tag == "HammerHead")
         {
-            string handHoldingHammer = other.transform.parent.gameObject.GetComponent<OVRGrabbable>().grabbedBy.ToString();
-            if(handHoldingHammer == "AvatarGrabberRight (OVRGrabber)")
+            if (isHittable)
             {
-                ControllerManager.CM.StartVibration(1, 1, 0.1f, OVRInput.Controller.RTouch);
-            } else if (handHoldingHammer == "AvatarGrabberLeft (OVRGrabber)")
-            {
-                ControllerManager.CM.StartVibration(1, 1, 0.1f, OVRInput.Controller.LTouch);
-            }
+           
+                string handHoldingHammer = other.transform.parent.gameObject.GetComponent<OVRGrabbable>().grabbedBy.ToString();
+                if(handHoldingHammer == "AvatarGrabberRight (OVRGrabber)")
+                {
+                    ControllerManager.CM.StartVibration(1, 1, 0.1f, OVRInput.Controller.RTouch);
+                } else if (handHoldingHammer == "AvatarGrabberLeft (OVRGrabber)")
+                {
+                    ControllerManager.CM.StartVibration(1, 1, 0.1f, OVRInput.Controller.LTouch);
+                }
 
+                isHittable = false;
+
+            
                 tracker.TakeHit();
-            generator.TakeHit(hitboxIndex);
-            GameObject hitInstance = GameObject.Instantiate(hitParticle, other.ClosestPointOnBounds(transform.position), Quaternion.identity);
-            hitInstance.transform.rotation = Quaternion.LookRotation(transform.up,Vector3.up);
+                generator.TakeHit(hitboxIndex);
+                GameObject hitInstance = GameObject.Instantiate(hitParticle, other.ClosestPointOnBounds(transform.position), Quaternion.identity);
+                hitInstance.transform.rotation = Quaternion.LookRotation(transform.up, Vector3.up);
+            }
 
         }
     }
