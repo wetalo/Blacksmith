a1aee29f7e6ba2d11e1f121131b7c2878c06121a
Dan <danielswieca@gmail.com>
2019-04-20 13:49:08 -0400

    Added feedback on good hit

--- FinalProject2/Assets/Scripts/GameManager.cs
+++ FinalProject2/Assets/Scripts/GameManager.cs
@@ -127,4 +127,6 @@ public class GameManager : MonoBehaviour {
     {
         return koreoGraphy.GetLatestSampleTime();
     }
+
+
 }
---
+++ FinalProject2/Assets/Scripts/IndicatorHitbox.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndicatorHitbox : MonoBehaviour
+{
+    public Indicator greenIndicator;
+    public GameObject hitParticle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "HammerHead" && greenIndicator.activated)
+        {
+            string handHoldingHammer = other.transform.parent.gameObject.GetComponent<OVRGrabbable>().grabbedBy.ToString();
+            if (handHoldingHammer == "AvatarGrabberRight (OVRGrabber)")
+            {
+                ControllerManager.CM.StartVibration(1, 1, 0.1f, OVRInput.Controller.RTouch);
+            }
+            else if (handHoldingHammer == "AvatarGrabberLeft (OVRGrabber)")
+            {
+                ControllerManager.CM.StartVibration(1, 1, 0.1f, OVRInput.Controller.LTouch);
+            }
+            GameObject hitInstance = GameObject.Instantiate(hitParticle, other.ClosestPointOnBounds(transform.position), Quaternion.identity);
+            hitInstance.transform.rotation = Quaternion.LookRotation(transform.up, Vector3.up);
+        }
+    }
+}
